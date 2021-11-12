05ae3a0c54d7a4e6f5a05bd65aba280f978a9b02
Oluwakemi-GT <oluwakemi.o@hotmail.co.uk>
2021-11-12 11:14:59 +0000

    installing packapges and adding accessories button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import AccessoriesButton from './components/accessories-button';
 
 function App() {
   return (
@@ -15,9 +16,14 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Alpaca World
         </a>
       </header>
+
+      <p>
+      <AccessoriesButton/>
+      </p>
+
     </div>
   );
 }
